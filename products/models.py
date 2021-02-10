22abe549474ad576e104c788e9fa976ff20bf250
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-10 01:26:59 +0000

    product model added

--- products/models.py
+++ products/models.py
@@ -63,3 +63,19 @@ class Colours (models.Model):
     def __str__(self):
         return '{}, {}'.format(self.name, self.product_id)
 
+class Product(models.Model):
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(null=True, max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+
+    def __str__(self):
+         return '{}, {}'.format(self.name, self.price_list)
+
