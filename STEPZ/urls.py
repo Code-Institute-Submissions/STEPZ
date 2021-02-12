5ab7487c98b7b29abbfaf46e802032ea64e66671
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-12 00:29:20 +0000

    template layout for shopping bag added

--- STEPZ/urls.py
+++ STEPZ/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
