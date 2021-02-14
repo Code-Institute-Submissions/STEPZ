e490a18c2edaa44464acaa5b449bc3e2ddffb6f5
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-14 23:49:15 +0000

    orderlineitem added to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
