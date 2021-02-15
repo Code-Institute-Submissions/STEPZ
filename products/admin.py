0f2fb116a862db69d6df94a8ac95e734e15c5794
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-15 13:42:48 +0000

    Price_list model added, and registered in admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Colours
+from .models import Product, Category, Colours, Price_list
 
 
 # Register your models here.
@@ -24,6 +24,14 @@ class ColoursAdmin(admin.ModelAdmin):
     )
 
 
+class Price_listAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'product_id',
+    )
+
+
+admin.site.register(Price_list, Price_listAdmin)
 admin.site.register(Colours, ColoursAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
