a369a6fb40ecaf8999a0aabea59477d807d5af81
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-16 18:04:08 +0000

    stripe secret key and public key added

--- STEPZ/settings.py
+++ STEPZ/settings.py
@@ -172,3 +172,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STANDARD_DELIVERY = 3
+
+# Stripe
+STANDARD_DELIVERY = 3
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
