9513403d18e657137a6742ee07d453e5b3c267d0
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-16 18:45:15 +0000

    checkout messaged added

--- checkout/views.py
+++ checkout/views.py
@@ -51,6 +51,17 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
+
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
