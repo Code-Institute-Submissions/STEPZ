1350384d3ecd9dd98eab34da9b63ff4f8f700823
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-17 17:28:41 +0000

    handy payement failed function added

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -23,3 +23,10 @@ class StripeWH_Handler:
             content=f'Webhook received: {event["type"]}',
             status=200)
 
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
