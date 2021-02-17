faa7e2335fd9249655b1ad791e2a2dd8862a480e
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-17 13:00:19 +0000

    price list and colours model removed

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Product, Category, Colours, Price_list
+from .models import Product, Category
 
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name',
+        'sku',
         'category',
         'price',
         'image',
     )
+
+    ordering = ('sku',)
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -17,22 +20,7 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-class ColoursAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'product_id',
-    )
 
-
-class Price_listAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'product_id',
-    )
-
-
-admin.site.register(Price_list, Price_listAdmin)
-admin.site.register(Colours, ColoursAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 
--- products/models.py
+++ products/models.py
@@ -17,53 +17,6 @@ class Category (models.Model):
         return self.name
 
 
-class Price_list (models.Model):
-
-    class Meta:
-        verbose_name_plural = 'PriceList'
-
-    price_choices = (
-        ('over_50','over_50'),
-        ('over_100','over_100'),
-        ('over_150','over_150'),
-        ('over_200','over_200'),
-        ('over_250','over_250'),
-        ('over_300','over_300'),
-    )
-
-    product_id = models.ForeignKey('Product', null=True,blank=True, on_delete=models.SET_NULL)
-    name =MultiSelectField(choices=price_choices, null=True)
-
-    def __str__(self):
-        return '{}, {}'.format(self.name, self.product_id)
-
-
-class Colours (models.Model):
-
-    class Meta:
-        verbose_name_plural = 'colours'
-
-    colour_choices = (
-        ('Brown','Brown'),
-        ('Black','Black'),
-        ('White','White'),
-        ('Red','Red'),
-        ('Blue','Blue'),
-        ('Yellow','Yellow'),
-        ('Orange','Orange'),
-        ('Purple','Purple'),
-        ('Green','Green'),
-        ('Pink','Pink'),
-        ('Gray','Gray'),
-    )
-
-    name = MultiSelectField(choices=colour_choices, null=True)
-    product_id = models.ForeignKey('Product', null=True,blank=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return '{}, {}'.format(self.name, self.product_id)
-
-
 class Product(models.Model):
 
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
