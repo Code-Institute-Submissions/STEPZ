b288aaea534df289dfc77ddedd46e55eab89b60c
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-18 22:23:15 +0000

    widgets added to customise image field

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product
 
 
@@ -7,3 +8,5 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
