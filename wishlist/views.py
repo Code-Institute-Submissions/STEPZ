4fd328df88e40a7543384271cc0a05410e9bf9b6
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-19 16:01:46 +0000

    view wishlist function created and urls added

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Wishlist, WishItems
 
 # Create your views here.
+
+
+def view_wishlist(request):
+
+    return render(request, 'wishlist/wishlist.html')
+
+
