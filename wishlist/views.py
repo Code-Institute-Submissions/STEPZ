20e9405e8acc09aa39f71af756449dcc0a54dec8
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-19 21:43:18 +0000

    view_wishlist function created

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Wishlist, WishItems
+from profiles.models import UserProfile
 
 # Create your views here.
 
 
 def view_wishlist(request):
+    """ Added a views to show the Wishlist """
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = get_object_or_404(Wishlist, user=user)
+    wishlist_items = get_list_or_404(WishItems, wishlist=wishlist)
+    print(wishlist_items)
 
-    return render(request, 'wishlist/wishlist.html')
+    context = {
+        'user': user,
+        'wishlist': wishlist,
+        'wishlist_items': wishlist_items,
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
 
 
