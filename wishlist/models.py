eaf043a88d988b4b39a3575ee27c0877fee33c92
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-20 19:03:48 +0000

    Wishitems and wishlist model deleted, Userwishlist registred to admin

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,21 +3,14 @@ from profiles.models import UserProfile
 from products.models import Product
 
 
-class Wishlist(models.Model):
-
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, null=True, blank=False, related_name='wishlist')
-    wishlist_products = models.ManyToManyField(Product, through='wishItems')
-
-    def __str__(self):
-        return f'Wishlist ({self.user},)'
-
-
-class WishItems(models.Model):
-    wishlist = models.ForeignKey(Wishlist, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist_items' )
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE, related_name='product_wishlist')
+class UserWishlist(models.Model):
+    user = models.ForeignKey(UserProfile, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist_owner')
+    products = models.ManyToManyField(Product)
 
     def __str__(self):
-        return self.product.name
+        return self.user.name
+
+
 
 
 
