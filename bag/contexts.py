b21be3b619c0c24a8808eebd95602de99714d1b6
Tabita99 <tabita.m@hotmail.co.uk>
2021-02-27 20:26:38 +0000

    standard and free  delivery and threshhold added

--- bag/contexts.py
+++ bag/contexts.py
@@ -33,10 +33,12 @@ def bag_contents(request):
                     'size': size,
                 })
 
-    if total > 0:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY/100)
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
+        free_delivery_delta = 0
 
     grand_total = delivery + total
 
@@ -44,10 +46,10 @@ def bag_contents(request):
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'delivery_cost': settings.STANDARD_DELIVERY,
         'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-
     }
 
     return context
